1a721c226f44982f95bda815244baad2842bbd6b
Dmitry Natalchuk <next1996@ukr.net>
2022-07-12 16:50:42 +0300

    add link js css

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+const galleryList = document.querySelector(".gallery")
+//console.log(galleryList)
+
+const createGallery = ({preview,original,description}) => 
+`<div class="gallery__item">
+<a class="gallery__link" 
+href=${original}>
+  <img
+    class="gallery__image"
+    src=${preview}
+    data-source=${original}
+    alt=${description}
+  />
+</a>
+</div>`;
+const galleryReduce = galleryItems
+.reduce((acc, item) => acc + createGallery(item),"");
+
+galleryList.insertAdjacentHTML("afterbegin",galleryReduce);
+
+const divGalleryItems = galleryList.querySelector(".gallery__item")
+//console.log(divGalleryItems.nodeName)
+
+function eventClickGallery (event) {
+    event.preventDefault()
+   if(event.target.nodeName !== "DIV"){
+    return
+   }
+  
+}
+
+galleryList.addEventListener("click",eventClickGallery)
