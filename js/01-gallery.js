731b310abec90e3e16efd295dfe374921954c8cd
Dmitry Natalchuk <next1996@ukr.net>
2022-07-13 18:11:51 +0300

    test 1 hw work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,35 +1,91 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryList = document.querySelector(".gallery")
+const gallery = document.querySelector(".gallery");
+//console.log(gallery)
+
+const imagesList = createGallery(galleryItems);
+//console.log(imagesList)
+
+gallery.insertAdjacentHTML("beforeend",imagesList);
+
+function createGallery(images) {
+  return galleryItems.map(({preview, original, description }) => {
+   return `<div class="gallery__item">
+      <a class="gallery__link"
+       href="${images.original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;}).join("")
+}
+const clickEvent = (event) => {
+  event.preventDefault();
+  if(event.target.classList.contains("gallery"))
+    return;
+  const source = event.target.dataset.source
+  const showImages = basicLightbox.create(`
+  <img src="${source}"width="800" height="600">`);
+
+   showImages.show();
+  // window.addEventListener("keydown",closeKey)
+};
+/* function closeKey (event) {
+   if(event.code === "Escape") {
+    
+   }
+} */
+gallery.addEventListener("click",clickEvent)
+
+
+
+//========================================================
+/* const galleryList = document.querySelector(".gallery")
 //console.log(galleryList)
 
 const creatElementImg = ({preview,original,description}) => 
-    `<div class="gallery__item"
-    <a class="gallery__link" 
-    href=${original}>
-      <img
-        class="gallery__image"
-        src=${preview}
-        data-source=${original}
-        alt=${description}
-      />
-    </a>
-    </div>`;
+`<div class="gallery__item"
+<a class="gallery__link" 
+href=${preview}>
+  <img
+    class="gallery__image"
+    src=${preview}
+    data-source=${original}
+    alt=${description}
+  />
+  </a>
+  </div>`;
 
-const galleryReduce = galleryItems.reduce((acc, item) => acc + creatElementImg(item),"");
+const galleryReduce= galleryItems.reduce((acc, item) => acc + creatElementImg(item),"");
 
-galleryList.insertAdjacentHTML("afterbegin",galleryReduce).show();
+galleryList.insertAdjacentHTML("afterbegin",galleryReduce);
+
 
 function eventClickGallery (event) {
     event.preventDefault()
    if(event.target.nodeName !== "DIV"){
     return
    }
-   event.show()
-} 
-galleryList.addEventListener("click",eventClickGallery)
+   event.target.create(`
+   <div class="gallery__item"
+<a class="gallery__link" 
+href=${preview}>
+  <img
+    class="gallery__image"
+    src=${preview}
+    data-source=${original}
+    alt=${description}
+  />
+  </a>
+  </div>`).show()
 
+} 
+galleryList.addEventListener("click",eventClickGallery)  */
+  
 //===============================================================================
 //console.log(creatElementImg)
 
