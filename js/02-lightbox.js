a71e4e0853fbba8aeef8c6be58ef6098f9a0882c
Dmitry Natalchuk <next1996@ukr.net>
2022-07-13 20:24:56 +0300

    add final hw 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
 
+const gallery = document.querySelector(".gallery");
+//console.log(gallery)
+
+const imagesList = createGallery(galleryItems);
+//console.log(imagesList)
+gallery.insertAdjacentHTML("beforeend",imagesList);
+
+function createGallery(images) {
+  return galleryItems.map(({preview, original, description }) => {
+   return` <a class="gallery__item"
+    href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`;})
+.join("")
+  }
+//песня
+  const galleryImg = new SimpleLightbox(".gallery a", {
+    //captionType : "alt",
+    captionsData: "alt",
+   // docClose: false,
+   // close : false,
+    captionDelay: 250,
+    animationSpeed : 200,
+    maxZoom: 5,
+   // disableScroll : false,
+    
+  });
